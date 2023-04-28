20df360b12b0321eb1f04cbc6b62c82c7fee07fd
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-27 17:52:37 -0700

    initial seeds

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department(name)
+VALUES("Sales"),
+("Marketing"),
+("Engineering");
+
+INSERT INTO role(title, salary)
+VALUES("Manager",100),
+("Assistant", 10),
+("Engineer",50);
+
+INSERT INTO employee(first_name,last_name,manager_id)
+VALUES("Sarah","Cambell",1),
+("John","Johnson", 2),
+("Mark","Joy",1);
