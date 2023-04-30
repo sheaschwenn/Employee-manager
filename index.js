51da9c10e89d3946f646790582b5aa590e6546f7
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-30 14:38:39 -0700

    classes added for viewing dbs working on add function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const mainMenu = require("./server")
+const {mainMenu,dropMenu} = require("./server")
 
 
 
@@ -8,7 +8,7 @@ inquirer
     {
         type:"list",
         message:'What would you like to do?',
-        choices:["view all departments","view all employees","add a department","add a role","add an employee","update an employee role"],
+        choices:["view all departments","view all roles", "view all employees","add a department","add a role","add an employee","update an employee role"],
         name: 'main'
     },
     {
@@ -19,8 +19,8 @@ inquirer
     },
     {
         type:'input',
-        message:'What role would you like to add?',
-        name: 'addRole',
+        message:'What is the name of the role you would like to add?',
+        name: 'addTitle',
         when: (choice) => choice.main === "add a role"
     },
     {
@@ -30,17 +30,37 @@ inquirer
         when: (choice) => choice.main === "add a role"
     },
     {
-        type:'input',
-        message:'What department is the role you would like to add in?',
+        type:'list',
+        message:'Which department does the role belong to?',
+        choices: dropMenu,
         name: 'addDepartment',
         when: (choice) => choice.main === "add a role"
     },
     {
         type:'input',
-        message:'What employee would you like to add?',
-        name: 'addEmployee',
-        when: (choice) => choice.main === "add a department"
+        message:'First name of the employee you would like to add',
+        name: 'addFirstName',
+        when: (choice) => choice.main === "add an employee"
+    },
+    {
+        type:'input',
+        message:'Last name of the employee you would like to add',
+        name: 'addLastName',
+        when: (choice) => choice.main === "add an employee"
+    },
+    {
+        type:'input',
+        message:'What is the role of this employee?',
+        name: 'addERole',
+        when: (choice) => choice.main === "add an employee"
+    },
+    {
+        type:'input',
+        message:'Who is this employees manager?',
+        name: 'addEManager',
+        when: (choice) => choice.main === "add an employee"
     }
+
 ])
 .then((data) =>{
     mainMenu(data);
--- server.js
+++ server.js
@@ -1,39 +1,74 @@
-const express = require("express");
 const mysql = require("mysql2");
 const consoleTable = require("console.table")
+const Employee = require("./lib/employee");
+const Department = require('./lib/department');
+const Role = require('./lib/role');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user:'root',
+        user: 'root',
         password: 'MySQL23real',
         database: 'employee_db'
     },
     console.log("Connected to employee_db")
 );
-function mainMenu(data){
-if(data.main === "view all employees"){
-    db.query('SELECT * FROM employee', function(err,results){
-        console.table(results)
-    })
-}
-else if(data.main ==="add a department"){
-    db.query('INSERT INTO department(name) VALUES(?)',data.addDepartment, function(err,results){
-        db.query(`SELECT * FROM department`, (err,results2)=>{
-            console.table(results2)
-        })
+function mainMenu(data) {
+    switch (data.main) {
+        case 'view all departments':
+            new Department(data.main).selectAll()
+            break;
+        case 'view all employees':
+            new Employee(data.main).selectAll()
+            break;
+        case 'view all roles':
+            new Role(data.main).selectAll();
+            break;
+        case 'add a department':
+            db.query('INSERT INTO department(name) VALUES(?)', data.addDepartment, function (err, results) {
+                db.query(`SELECT * FROM department`, (err, results2) => {
+                    console.table(results2)
+                })
+            })
+        case 'add a role':
         
-})
-}
-else if (data.main === "view all departments"){
-    db.query(`SELECT * FROM department`, (err,results) =>{
-        console.table(results)
-    })
-}
-else if(data.main ===""){}
+        db.query('INSERT INTO role(title, salary, department_id) VALUES(?)',[data.addTitle,data.addSalary,1],(err,results)=>{
+            console.table(results)
+        })
+    }
 }
 
-module.exports = mainMenu;
+function dropMenu(){
+ 
+
+    db.query('SELECT name FROM department',(err,results)=>{
+       return results.name
+        })
+    }
+
+
+module.exports = {mainMenu, dropMenu};
+
+    // if(data.main === "view all employees"){
+    //     db.query('SELECT * FROM employee', function(err,results){
+    //         console.table(results)
+    //     })
+    // }
+    // else if(data.main ==="add a department"){
+    //     db.query('INSERT INTO department(name) VALUES(?)',data.addDepartment, function(err,results){
+    //         db.query(`SELECT * FROM department`, (err,results2)=>{
+    //             console.table(results2)
+    //         })
+
+    // })
+    // }
+    // else if (data.main === "view all departments"){
+    //     db.query(`SELECT * FROM department`, (err,results) =>{
+    //         console.table(results)
+    //     })
+    // }
+    // else if(data.main ===""){  }
+
+
