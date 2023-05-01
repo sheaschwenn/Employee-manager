d2aeb640ab90bfee9b59d0489f75bc1b4d47da47
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-30 17:43:19 -0700

    restructuring of prompts and funtions

--- index.js
+++ index.js
@@ -1,67 +1,92 @@
 const inquirer = require("inquirer");
-const {mainMenu,dropMenu} = require("./server")
+const { viewDepartments,viewEmployees,viewRoles,addDepartment,addRole, dropMenu } = require("./server")
 
 
 
 inquirer
-.prompt([
-    {
-        type:"list",
-        message:'What would you like to do?',
-        choices:["view all departments","view all roles", "view all employees","add a department","add a role","add an employee","update an employee role"],
-        name: 'main'
-    },
-    {
-        type:'input',
-        message:'What department would you like to add?',
-        name: 'addDepartment',
-        when: (choice) => choice.main === "add a department"
-    },
-    {
-        type:'input',
-        message:'What is the name of the role you would like to add?',
-        name: 'addTitle',
-        when: (choice) => choice.main === "add a role"
-    },
-    {
-        type:'input',
-        message:'What is the salary for the role you would like to add?',
-        name: 'addSalary',
-        when: (choice) => choice.main === "add a role"
-    },
-    {
-        type:'list',
-        message:'Which department does the role belong to?',
-        choices: dropMenu(),
-        name: 'addDepartment',
-        when: (choice) => choice.main === "add a role"
-    },
-    {
-        type:'input',
-        message:'First name of the employee you would like to add',
-        name: 'addFirstName',
-        when: (choice) => choice.main === "add an employee"
-    },
-    {
-        type:'input',
-        message:'Last name of the employee you would like to add',
-        name: 'addLastName',
-        when: (choice) => choice.main === "add an employee"
-    },
-    {
-        type:'input',
-        message:'What is the role of this employee?',
-        name: 'addERole',
-        when: (choice) => choice.main === "add an employee"
-    },
-    {
-        type:'input',
-        message:'Who is this employees manager?',
-        name: 'addEManager',
-        when: (choice) => choice.main === "add an employee"
-    }
+    .prompt([
+        {
+            type: "list",
+            message: 'What would you like to do?',
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
+            name: 'main'
+        }
+    ])
+    .then((data) => {
+        switch (data.main) {
+            case 'view all departments':
+                viewDepartments(data)
+                break;
+            case 'view all roles':
+                viewEmployees(data)
+                break;
+            case 'view all employees':
+                viewRoles(data)
+                break;
+            case 'add a department':
+                addDepartment(data)
+            break;
+            case 'add a role':
+                addRole()
+            break;
+            case 'add an employee':
+            break;
+            case 'update an employee role':
+            break;
 
-])
-.then((data) =>{
-    mainMenu(data);
-})
+        }
+    })
+//         {
+//             type: 'input',
+//             message: 'What is the name of the role you would like to add?',
+//             name: 'addTitle',
+//             when: (choice) => choice.main === "add a role"
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is the salary for the role you would like to add?',
+//             name: 'addSalary',
+//             when: (choice) => choice.main === "add a role"
+//         },
+//         {
+//             type: 'list',
+//             message: 'Which department does the role belong to?',
+//             choices: dropMenu(),
+//             name: 'addDepartment',
+//             when: (choice) => choice.main === "add a role"
+//         },
+//         {
+//             type: 'input',
+//             message: 'First name of the employee you would like to add',
+//             name: 'addFirstName',
+//             when: (choice) => choice.main === "add an employee"
+//         },
+//         {
+//             type: 'input',
+//             message: 'Last name of the employee you would like to add',
+//             name: 'addLastName',
+//             when: (choice) => choice.main === "add an employee"
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is the role of this employee?',
+//             name: 'addERole',
+//             when: (choice) => choice.main === "add an employee"
+//         },
+//         {
+//             type: 'input',
+//             message: 'Who is this employees manager?',
+//             name: 'addEManager',
+//             when: (choice) => choice.main === "add an employee"
+//         },
+//         {
+//             type: 'input',
+//             message: 'Which employee would you like to update?',
+//             name: 'addUName',
+//             when: (choice) => choice.main === "update an employee"
+//         },
+
+// ])
+//     .then((data) => {
+//         mainMenu(data);
+//     })
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const consoleTable = require("console.table")
 const Employee = require("./lib/employee");
 const Department = require('./lib/department');
 const Role = require('./lib/role');
+const inquirer = require("inquirer");
 
 
 
@@ -15,41 +16,85 @@ const db = mysql.createConnection(
     },
     console.log("Connected to employee_db")
 );
-function mainMenu(data) {
-    switch (data.main) {
-        case 'view all departments':
-            new Department(data.main).selectAll()
-            break;
-        case 'view all employees':
-            new Employee(data.main).selectAll()
-            break;
-        case 'view all roles':
-            new Role(data.main).selectAll();
-            break;
-        case 'add a department':
+
+function dropMenu() {
+
+    db.query('SELECT name FROM department', (err, results) => {
+        let departments = results.name
+        return departments
+    })
+}
+function viewDepartments(data) {
+    return new Department(data.main).selectAll()
+}
+
+function viewEmployees(data) {
+    return new Employee(data.main).selectAll()
+}
+
+function viewRoles(data) {
+    return new Role(data.main).selectAll();
+}
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What department would you like to add?',
+                name: 'addDepartment'
+            }
+        ])
+        .then((data) => {
             db.query('INSERT INTO department(name) VALUES(?)', data.addDepartment, function (err, results) {
                 db.query(`SELECT * FROM department`, (err, results2) => {
                     console.table(results2)
                 })
             })
-        case 'add a role':
-        
-        db.query('INSERT INTO role(title, salary, department_id) VALUES(?)',[data.addTitle,data.addSalary,1],(err,results)=>{
-            console.table(results)
         })
+}
+function addRole() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the name of the role you would like to add?',
+        name: 'addTitle',
+        when: (choice) => choice.main === "add a role"
+    },
+    {
+        type: 'input',
+        message: 'What is the salary for the role you would like to add?',
+        name: 'addSalary',
+        when: (choice) => choice.main === "add a role"
+    },
+])
+.then(
+    inquirer
+    .prompt([
+    {
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        choices: dropMenu(),
+        name: 'addDepartment',
+        when: (choice) => choice.main === "add a role"
     }
+])
+)
+.then((data)=>{
+    db.query('INSERT INTO role(title, salary, department_id) VALUES(?)', [data.addTitle, data.addSalary, 1], (err, results) => {
+        console.table(results)
+    })
+}
+)
 }
 
-function dropMenu(){
- 
-
-    db.query('SELECT name FROM department',(err,results)=>{
-       return results.name
-        })
-    }
 
 
-module.exports = {mainMenu, dropMenu};
+
+
+
+module.exports = { viewDepartments, viewEmployees, viewRoles, addDepartment, addRole, dropMenu };
 
     // if(data.main === "view all employees"){
     //     db.query('SELECT * FROM employee', function(err,results){
